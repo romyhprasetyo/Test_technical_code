814d9ee3bdbcf0561dcf6c2f9694bc34751370b7
romyhprasetyo@gmail.com <romyhprasetyo@gmail.com>
2022-11-30 14:41:51 +0700

    finishing

--- index.php
+++ index.php
@@ -5,23 +5,61 @@
                 margin-bottom: 15px;
                 padding: 10px;
             }
+            .error{
+                color: red;
+            }
+            .success{
+                color: green;
+            }
+
+            #result{
+                margin: 2rem 0;
+            }
         </style>
     </head>
     <body>
-        <form action="">
-            <input type="text" placeholder="Input angka" name="submission"/>
-            <div class="btn-group">
-                <button>Generate segitiga</button>
-                <button>Generate Bilangan Ganjil</button>
-                <button>Generate Bilangan Prima</button>
-            </div>
-        </form>
+
+        <div>
+            <input type="text" id="submission" placeholder="Input angka" name="submission"/>
+        </div>
+        <span class="err-message"></span>
+        <br/>
+        <div class="btn-group">
+            <button class="btn-submit" data-id="segitiga" onclick="submission(this)">Generate segitiga</button>
+            <button class="btn-submit" data-id="ganjil" onclick="submission(this)">Generate Bilangan Ganjil</button>
+            <button class="btn-submit" data-id="prima" onclick="submission(this)">Generate Bilangan Prima</button>
+        </div>
 
         <div id="result"></div>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.js"></script>
         <script>
-            $('#result').html('test');
+            function submission(obj){
+                var data_attr = obj.getAttribute('data-id');
+                var value = parseInt($('#submission').val());
+                $.ajax({
+                    url : 'http://localhost/test-new/libs/NumberController.php?type='+data_attr+'&value='+value,
+                    method : 'POST',
+                    success: function (data, msg){
+                        console.log(data);
+                        $('#result').html(`${data}`);
+                    }
+                });
+            }
+
+
+            $('#submission').on('keyup', function (e){
+                var regex = /^\d*[.]?\d*$/;
+                var value = $(this).val();
+
+                if(regex.test(value)){
+                    $('.err-message').html('valid').removeClass('error').addClass('success');
+                    $('.btn-submit').removeAttr('disabled');
+                }else if(!regex.test(value)){
+                    $('.err-message').html('not valid').addClass('error').removeClass('success');
+                    $('.btn-submit').attr('disabled', true);
+                }
+            });
         </script>
     </body>
 </html>
--- libs/NumberController.php
+++ libs/NumberController.php
@@ -1,2 +1,30 @@
 <?php
 
+$actionType = $_GET['type'];
+$value = (int) $_GET['value'];
+
+if($actionType == 'segitiga'){
+    $segitiga = str_split($value);
+    //echo json_encode($segitiga);
+    for($i=0; $i<count($segitiga); $i++){
+        for($j=0; $j<$i; $j++){
+            echo '0';
+        }
+        echo $segitiga[$i].'<br>';
+    }
+}elseif($actionType == 'ganjil'){
+    for($i = 1; $i <= $value; $i++){
+        if($i % 2 == 1){
+            echo $i." ";
+        }
+    }
+}elseif($actionType == 'prima'){
+    for($i = 1; $i <= $value; $i++){
+        if(!preg_match( '/^1?$|^(11+?)\1+$/x', str_repeat('1', $i))){
+            echo $i." ";
+        }
+    }
+}
+else{
+    echo $actionType.'-'.$value;
+}
