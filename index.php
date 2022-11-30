e9ed9dc2f95b2e771d55fc38050b6f31070822fa
romyhprasetyo@gmail.com <romyhprasetyo@gmail.com>
2022-11-30 13:22:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <style>
+            input{
+                margin-bottom: 15px;
+                padding: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="">
+            <input type="text" placeholder="Input angka" name="submission"/>
+            <div class="btn-group">
+                <button>Generate segitiga</button>
+                <button>Generate Bilangan Ganjil</button>
+                <button>Generate Bilangan Prima</button>
+            </div>
+        </form>
+
+        <div id="result"></div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.js"></script>
+        <script>
+            $('#result').html('test');
+        </script>
+    </body>
+</html>
